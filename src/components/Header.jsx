8c630745e61449f888c94a5e7c468f637658d393
Martin-Rouault <martin.rlt24@gmail.com>
2025-03-03 13:58:27 +0100

    Created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="nav">
+            <img src="/logo.webp" alt="" />
+            <a href="#">Home</a>
+        </header>
+    );
+}
