02b6aad1735a7f1432d3eb9bcd72b4e33cd0acec
Martin-Rouault <martin.rlt24@gmail.com>
2025-03-04 10:41:47 +0100

    removed unecessary component

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,17 +1,16 @@
-import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
 export default function Dish({ name, prix, image }) {
     return (
-                <Card style={{ width: "18rem" }}>
-                    <Card.Img variant="top" src={image} className="dish-img" />
-                    <Card.Body>
-                        <Card.Title>{name}</Card.Title>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>{prix}</ListGroup.Item>
-                    </ListGroup>
-                </Card>
+        <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={image} className="dish-img" />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroup.Item>{prix}</ListGroup.Item>
+            </ListGroup>
+        </Card>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import Navigation from "./Navbar";
-import Basic from "./Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Header() {
     return (
         <header>
-            <Navigation />
+            <Navbar className="nav">
+                <Container className="d-flex justify-content-between align-items-center">
+                    <Navbar.Brand href="#home">
+                        <img src="/logo.webp" className="brand" alt="" />
+                    </Navbar.Brand>
+                    <Nav className="">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     );
 }
